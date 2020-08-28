5f2a23efc5e866e22d39deb3093392a3e0f73e7a
Chris <32127270+cjhawk7@users.noreply.github.com>
2020-08-28 11:32:14 -0700

    more speed

--- carousel.js
+++ carousel.js
@@ -1,13 +1,48 @@
 !(function ($) {
   window.onload = function () {
-    var gradComment = [["MUCH DATA HERE"]];
-    var randoNum = Math.floor(Math.random() * 1200 + 1);
+    var gradComment = [
+      [
+        '<p class="student">Tom Cruise</p><p class="quote">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p><p class="author">Lorem ipsum</p>',
+      ],
+      [
+        '<p class="student">Denzel Washington</p><p class="quote">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p><p class="author">Lorem ipsum</p>',
+      ],
+      [
+        '<p class="student">Brad Pitt</p><p class="quote">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p><p class="author">Lorem ipsum</p>',
+      ],
+      [
+        '<p class="student">Harrison Ford</p><p class="quote">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p><p class="author">Lorem ipsum</p>',
+      ],
+      [
+        '<p class="student">Michael Jordan</p><p class="quote">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p><p class="author">Lorem ipsum</p>',
+      ],
+      [
+        '<p class="student">Will Smith</p><p class="quote">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p><p class="author">Lorem ipsum</p>',
+      ],
+      [
+        '<p class="student">Leonardo Dicaprio</p><p class="quote">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p><p class="author">Lorem ipsum</p>',
+      ],
+      [
+        '<p class="student">George Washington</p><p class="quote">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p><p class="author">Lorem ipsum</p>',
+      ],
+      [
+        '<p class="student">Abraham Lincoln</p><p class="quote">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p><p class="author">Lorem ipsum</p>',
+      ],
+      [
+        '<p class="student">Franklin Roosevelt</p><p class="quote">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p><p class="author">Lorem ipsum</p>',
+      ],
+      [
+        '<p class="student">Barack Obama</p><p class="quote">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p><p class="author">Lorem ipsum</p>',
+      ],
+      [
+        '<p class="student">Zeus</p><p class="quote">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p><p class="author">Lorem ipsum</p>',
+      ],
+    ];
+
     var comment = $(".one");
     var commentTwo = $(".two");
     var commentThree = $(".three");
-    $(comment).append(gradComment[i]);
-    $(commentTwo).append(gradComment[i + 1]);
-    $(commentThree).append(gradComment[i + 2]);
+    console.log(gradComment);
     function postGrad(i) {
       var quote = Object.values($(".quote"));
       quote.forEach(function (text) {
@@ -16,7 +51,7 @@
         }
       });
       if (gradComment[i] === undefined) {
-        postGrad(randoNum);
+        postGrad(0);
       }
       if (i < gradComment.length) {
         setTimeout(function () {
@@ -28,9 +63,9 @@
           $(commentTwo).append(gradComment[i + 1]);
           $(commentThree).append(gradComment[i + 2]);
           postGrad(i + 3);
-        }, 10000);
+        }, 4000);
       }
     }
-    postGrad(randoNum);
+    postGrad(0);
   };
 })(jQuery);
