7ae3cf13a83e5916ce9711b4364b28f8fa5bd6e3
Chris <32127270+cjhawk7@users.noreply.github.com>
2021-05-27 11:45:24 -0700

    updated it carousel algorithm and added search function

--- carousel.js
+++ carousel.js
@@ -39,10 +39,52 @@
       ],
     ];
 
+    $(".search-button").click(function (e) {
+      e.preventDefault();
+      var slicedQuote;
+      $(".student-searched").remove();
+      $(".quote-searched").remove();
+      $(".author-searched").remove();
+      $(".error-msg").remove();
+      var lastName = $("#fname").val().toLowerCase().replaceAll("[-+.^:,]", "");
+      gradComment.forEach(function (comment) {
+        var slicedComment = $(comment[0]).slice();
+        if (lastName === "") {
+          return;
+        }
+        if (comment) {
+          if (slicedComment[0]) {
+            if (slicedComment[0].innerText.toLowerCase().replaceAll("[-+.^:,]", "").includes(lastName)) {
+              console.log($(".error-msg"));
+              slicedQuote = '<p class="quote-searched">' + slicedComment[1].innerText + "</p>";
+              slicedAuthor = '<p class="author-searched"> - ' + slicedComment[2].innerText + "</p>";
+              if (slicedAuthor === "undefined") {
+                slicedComment[2].innerText = "Yours truly"
+              }
+              $(slicedAuthor).insertAfter(".search-button")
+              $(slicedQuote).insertAfter(".search-button")
+              $(".error-msg").remove();
+            } if (slicedComment[0].innerText.toLowerCase().replaceAll("[-+.^:,]", "").includes(lastName) === false) {
+              var errorMessage = '<p class="error-msg">Could not find any messages!</p>'
+              $(".error-msg").remove();
+              $(errorMessage).insertAfter(".search-button")
+              setTimeout(function () {
+                $(".error-msg").remove();
+              }, 1000);
+            }
+          }
+        }
+      })
+    })
+
+    var randoNum = Math.floor(Math.random() * 195 + 1);
     var comment = $(".one");
     var commentTwo = $(".two");
     var commentThree = $(".three");
-    console.log(gradComment);
+    $(comment).append(gradComment[0]);
+    $(commentTwo).append(gradComment[1]);
+    $(commentThree).append(gradComment[2]);
+    var count = 0;
     function postGrad(i) {
       var quote = Object.values($(".quote"));
       quote.forEach(function (text) {
@@ -62,10 +104,12 @@
           $(comment).append(gradComment[i]);
           $(commentTwo).append(gradComment[i + 1]);
           $(commentThree).append(gradComment[i + 2]);
-          postGrad(i + 3);
-        }, 4000);
+          count++
+          console.log($(".student").text())
+          postGrad(i + 2);
+        }, 10000);
       }
     }
-    postGrad(0);
+    postGrad(randoNum);
   };
 })(jQuery);
